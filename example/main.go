3c461ea9bf864d56a27c5322415a81b31e5d9e2c
Michael Persson <michael.ake.persson@gmail.com>
2018-05-01 22:07:52 -0500

    fix lint errors in example

--- example/main.go
+++ example/main.go
@@ -4,24 +4,29 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/mickep76/kvstore/cmp"
+	"github.com/mickep76/cmp"
 )
 
+// Car type for examp;e.
 type Car struct {
 	Manufacturer string
 	Model        string
 }
 
+// Cars multiple cars.
 type Cars []*Car
 
+// String representation of car.
 func (c Car) String() string {
 	return c.Manufacturer + " " + c.Model
 }
 
+// Eq interface for comparison.
 func (c Car) Eq(b interface{}) (bool, error) {
 	return c.String() == b.(Car).String(), nil
 }
 
+// Lt interface for comparison.
 func (c Car) Lt(b interface{}) (bool, error) {
 	return c.String() < b.(Car).String(), nil
 }
